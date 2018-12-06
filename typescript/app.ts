35e6f005d048fb9be26f99246a3bd1e9c0d2694e
Jeferson Almeida <re035108@resource.com.br>
2018-12-06 21:10:19 -0200

    typescript

---
+++ typescript/app.ts
@@ -0,0 +1,14 @@
+let mensagem: string = 'Seja bem vindo';
+let temporadasFriends: number = 10;
+let estudandoAngular: boolean = true;
+let listaDeFrutas: string[] = ['Uva', 'Banana', 'Abacaxi'];
+let notasProvas: number[] = [7.5, 8, 9];
+
+estudandoAngular = false;
+
+
+// let ola = (nome: string, sobrenome: string) => {
+//     console.log('Olá ' + nome + ' ' + sobrenome);
+// }
+
+// ola('Jeferson', 'Almeida');
---
+++ typescript/dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var mensagem = 'Seja bem vindo';
+var temporadasFriends = 10;
+var estudandoAngular = true;
+var listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
+var notasProvas = [7.5, 8, 9];
+estudandoAngular = false;
+// let ola = (nome: string, sobrenome: string) => {
+//     console.log('Olá ' + nome + ' ' + sobrenome);
+// }
+// ola('Jeferson', 'Almeida');
