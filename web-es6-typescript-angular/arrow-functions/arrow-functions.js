5f4e1ac02269828be79cab317a74a612d6e2fece
Jeferson Almeida <re035108@resource.com.br>
2019-09-05 21:43:39 -0300

    Arrow Functions

---
+++ web-es6-typescript-angular/arrow-functions/arrow-functions.js
@@ -0,0 +1,20 @@
+
+
+var dobroDoValor = numero => {
+    let resultado = numero * 2;
+    return resultado;
+}
+console.log(dobroDoValor(7));
+
+
+
+// var dobroDoValor = numero => numero * 2;    
+// console.log(dobroDoValor(7));
+
+
+
+// var dobroDoValor = function(numero) {
+//     return numero * 2;
+// }
+
+// console.log(dobroDoValor(7));
