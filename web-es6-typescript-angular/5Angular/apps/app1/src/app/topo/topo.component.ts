509567001c656d2ecdf23bdd9b7ce32e89226932
Jeferson Almeida <jeferson.almeida@resourceit.com>
2020-04-06 11:33:40 -0300

    add component styles

--- web-es6-typescript-angular/5Angular/apps/app1/src/app/topo/topo.component.ts
+++ web-es6-typescript-angular/5Angular/apps/app1/src/app/topo/topo.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-topo',
-  templateUrl: './topo.component.html'
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.scss']
 })
 
 export class TopoComponent { }
