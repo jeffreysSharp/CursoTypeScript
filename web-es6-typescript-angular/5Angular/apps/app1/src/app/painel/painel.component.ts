15b9b2ab9ee688c65c4f9ce61361981a4f586b7b
Jeferson Almeida <jeferson.almeida@resourceit.com>
2020-04-15 11:57:51 -0300

    63. App1 - Estabelecendo a lógica das rodadas de frases

--- web-es6-typescript-angular/5Angular/apps/app1/src/app/painel/painel.component.ts
+++ web-es6-typescript-angular/5Angular/apps/app1/src/app/painel/painel.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.scss']
 })
+
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  frases: Frase[] = FRASES;
+  instrucao = 'Traduza a frase';
+  resposta = '';
+  rodada = 0;
+  rodadaFrase: Frase;
+
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log('this.rodadaFrase', this.rodadaFrase);
+  }
 
   ngOnInit() {
+  }
+
+  atualizaResposta(event: Event) {
+    this.resposta = (event.target as HTMLInputElement).value;
+  }
+
+  verificarResposta() {
+    console.log('Verificar resposta: ', this.resposta);
   }
 
 }
