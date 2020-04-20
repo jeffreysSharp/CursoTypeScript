038fd917ab9f56b98c5cb98460b2b65bb3503097
Jeferson Almeida <jeferson.almeida@resourceit.com>
2020-04-20 11:26:09 -0300

    66. App1 - @Input (property binding entre o componente painel e progresso)

--- web-es6-typescript-angular/5Angular/apps/app1/src/app/progresso/progresso.component.ts
+++ web-es6-typescript-angular/5Angular/apps/app1/src/app/progresso/progresso.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progresso',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressoComponent implements OnInit {
 
-  progresso = 25;
+  @Input() progresso = 0;
 
   constructor() { }
 
